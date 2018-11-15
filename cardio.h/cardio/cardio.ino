b464f59026280b71300894b9317740e82989e490
Paulpicotin <44963927+Paulpicotin@users.noreply.github.com>
2018-11-15 09:53:59 +0100

    Add files via upload

---
+++ cardio.h/cardio/cardio.ino
@@ -0,0 +1,32 @@
+int k =0;
+int moy =0;
+int tempsmoy=0;
+void arg (){
+  int temps=100 ;
+  int i =0;
+
+  while(temps != 20100){
+  long nombre = random(405,481); //ici je simule un capteur infrarouge captant une valeur entre 405 et 480 
+  delay(100);
+  
+  temps = temps + 100;
+  if (nombre > 470){ //cet fonction sert a incrémenter i le nombre de pulsation a chaque fois que notre capteur capte une valeur au dessu de 470
+    i++; 
+    
+    }
+ 
+  }
+tempsmoy++ ; // je fais la moyenne  de tout mes i pour afficher mes valeurs 
+k=k+20;
+moy=moy+i;
+Serial.print(i*6);
+Serial.print(';');
+Serial.print(k);
+Serial.println();
+if (tempsmoy == 12){
+moy=moy/2;
+}
+
+}
+
+
---
+++ cardio.h/cardio/main.ino
@@ -0,0 +1,22 @@
+#include "cardio.h"
+
+int j=1;
+int i =0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);  
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+   // on appelle la fonction arg () contenu dans le cardio.h
+
+   for (j;j<=12;j++){  // cet boucle sert a arreté le programme tant que j != de 12 
+
+    arg();
+   
+    
+    
+  }
+}
